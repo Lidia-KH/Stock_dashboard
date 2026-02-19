a03fd7683148fcbfaaf136b20f1726b66a9bf09a
Lidia-KH <i.khelif@esi-sba.dz>
2026-02-19 12:53:56 +0100

    update

--- database.py
+++ database.py
@@ -1,7 +1,8 @@
 from sqlmodel import SQLModel, create_engine
+import streamlit as st
 
 engine = create_engine("sqlite:///database.db", echo=False)
-
+@st.cache_resource
 def init_db():
     SQLModel.metadata.create_all(engine)
     
